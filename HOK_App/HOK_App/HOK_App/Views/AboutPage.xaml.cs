c43028707488151d0a0884e91bdd63850f36788d
Daniel Causer <danielcauser@gmail.com>
2018-10-07 14:09:18 -0400

    unadded files

---
+++ HOK_App/HOK_App/HOK_App/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace HOK_App.Views
+{
+    public partial class AboutPage : BasePage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
